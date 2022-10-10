eb62c9dce163efa3a14c4470a3114f247af58c3d
Jethro <estangkij@gmail.com>
2022-10-11 00:39:40 +0800

    Use .json instead of .csv for questions

--- js/script.js
+++ js/script.js
@@ -1,32 +1,15 @@
-const questions = document.getElementById("input_file");
-questions.addEventListener("change", displayQuiz, false);
-
 function displayQuiz() {
-  document.getElementById("prompt").style.display = "none";
   getData();
 }
 
-function getData() {
-  if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-    console.log('The File APIs are not fully supported in this browser.');
-    return;
-  }
-
-  if (!questions.files) {
-    console.log("This browser doesn't seem to support the `files` property of file inputs.");
-  } else if (!questions.files[0]) {
-    console.log("No file selected.");
-  } else {
-    let file = questions.files[0];
-    let fr = new FileReader();
-    fr.onload = receivedText;
-    fr.readAsText(file);
-
-    function receivedText() {
-      console.log(fr.result);
-      console.log(fr.result.split("\n"));
-
-      // Do additional processing here
-    }
-  }
+function readJSONfile(path) {
+  var request = new XMLHttpRequest();
+  request.open("GET", path, false);
+  request.send(null);
+  var JSONobject = JSON.parse(request.responseText);
+  return JSONobject;
 }
+
+const questions = readJSONfile("../res/questions.json");
+
+console.log(questions)

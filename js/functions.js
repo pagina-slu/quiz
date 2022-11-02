7cb0e1db7ed4f9ba5491e335ac27a6c8fcddebfa
iamjethrooo <estangkij@gmail.com>
2022-11-02 11:32:09 +0800

    Add getResponsesForCategory method

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,22 @@
 /*
     This file contains utility functions
 */
+
+// Removes all child nodes of a given element
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
+// Returns all responses
 function getResponses() {
     return JSON.parse(localStorage.getItem('responses')) || [];
+}
+
+// Returns all responses in a given category
+function getResponsesForCategory(_category) {
+    return getResponses().filter(({category}) => category === _category);
 }
 
 function clearLocalStorage() {
